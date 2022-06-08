493552ed92bf90613c0516c8585045677ed4743a
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-06-08 11:52:49 +0200

    service message

--- src/main/java/com/rotechnology/chat/service/ChatService.java
+++ src/main/java/com/rotechnology/chat/service/ChatService.java
@@ -1,5 +1,6 @@
 package com.rotechnology.chat.service;
 
+import com.rotechnology.chat.entity.Message;
 import com.rotechnology.chat.entity.User;
 
 import java.util.List;
@@ -10,6 +11,12 @@ public interface ChatService {
     User updateUSer(User user);
     void deleteUser(long id);
     List<User> getUsers();
+
+    Message addMessage(Message message);
+    Message updateMessage(Message message);
+    void deleteMessage(long id);
+    List<Message> getMessages();
+    Message findMessageById(long id);
 
 
 }
