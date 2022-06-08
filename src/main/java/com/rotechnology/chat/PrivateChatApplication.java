4fd0a0fa07452bb8f6a78dccf108984c85f09ab5
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-06-08 14:33:55 +0200

    starting bean

--- src/main/java/com/rotechnology/chat/PrivateChatApplication.java
+++ src/main/java/com/rotechnology/chat/PrivateChatApplication.java
@@ -1,13 +1,44 @@
 package com.rotechnology.chat;
 
+import com.rotechnology.chat.entity.Chat;
+import com.rotechnology.chat.entity.ChatKey;
+import com.rotechnology.chat.entity.Message;
+import com.rotechnology.chat.entity.User;
+import com.rotechnology.chat.service.ChatService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class PrivateChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrivateChatApplication.class, args);
+	}
+
+//	@Bean
+	CommandLineRunner run(ChatService chatService){
+		return args -> {
+			User U1 = new User(1L,"Matteo","root");
+			User U2 = new User(2L,"Giuseppe", "root");
+
+			chatService.addUser(U1);
+			chatService.addUser(U2);
+
+			Message M1 = new Message(1L,"ciao", LocalDate.now());
+			Message M2 = new Message(2L,"ciao, come stai?", LocalDate.now());
+
+			chatService.addMessage(M1);
+			chatService.addMessage(M2);
+
+			ChatKey ck = new ChatKey(1L,2L,1L);
+			chatService.addChat(new Chat(ck,U1,U2,M1));
+
+		};
+
 	}
 
 }
