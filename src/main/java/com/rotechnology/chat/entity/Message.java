cc9dd6b8a226025a1e5283ea6a98fc630c07a901
MatteoGamba1993 <97434603+MatteoGamba1993@users.noreply.github.com>
2022-06-08 10:34:19 +0200

    Message entity

---
+++ src/main/java/com/rotechnology/chat/entity/Message.java
@@ -0,0 +1,22 @@
+package com.rotechnology.chat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private LocalDate timestamp;
+
+}
