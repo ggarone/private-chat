4ac2fce77963e0459549fc11569452cd3d9d1525
MatteoGamba1993 <97434603+MatteoGamba1993@users.noreply.github.com>
2022-06-08 12:04:52 +0200

    user and message imp

--- src/main/java/com/rotechnology/chat/repository/MessageRepo.java
+++ src/main/java/com/rotechnology/chat/repository/MessageRepo.java
@@ -1,9 +1,14 @@
 package com.rotechnology.chat.repository;
 
 import com.rotechnology.chat.entity.Message;
+import com.rotechnology.chat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MessageRepo extends JpaRepository<Message,Long> {
+    Optional<Message> findMessageById(long id);
+    void deleteMessageById(long id);
 }
