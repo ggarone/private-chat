14a909bc18572dca8b4f7fd484ce46b6d9756306
MatteoGamba1993 <97434603+MatteoGamba1993@users.noreply.github.com>
2022-06-08 11:41:55 +0200

    user service

--- src/main/java/com/rotechnology/chat/service/ChatService.java
+++ src/main/java/com/rotechnology/chat/service/ChatService.java
@@ -1,5 +1,15 @@
 package com.rotechnology.chat.service;
 
+import com.rotechnology.chat.entity.User;
+
+import java.util.List;
+
 public interface ChatService {
+
+    User addUser(User user);
+    User updateUSer(User user);
+    void deleteUser(long id);
+    List<User> getUSers();
+
 
 }
